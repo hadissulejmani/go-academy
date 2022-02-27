037238b1dc88b3c02b170ca64df2cb473a37e99a
Hadis Sulejmani <hadissulejmani@outlook.com>
2022-02-27 13:32:26 +0100

    add: second week, fourth lecture, task02

--- week02/lecture04/task02/task02.go
+++ week02/lecture04/task02/task02.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Println("Bib")
+	cityValues := groupSlices(citiesAndPrices())
+	for key, value := range cityValues {
+		fmt.Println(key, value)
+	}
+}
+
+func citiesAndPrices() ([]string, []int) {
+	rand.Seed(time.Now().UnixMilli())
+	cityChoices := []string{"Berlin", "Moscow", "Chicago", "Tokyo", "London"}
+	dataPointCount := 100
+	// randomly choose cities
+	cities := make([]string, dataPointCount)
+	for i := range cities {
+		cities[i] = cityChoices[rand.Intn(len(cityChoices))]
+	}
+	prices := make([]int, dataPointCount)
+	for i := range prices {
+		prices[i] = rand.Intn(100)
+	}
+	return cities, prices
+}
+
+func groupSlices(keySlice []string, valueSlice []int) map[string][]int {
+	result := make(map[string][]int, 5)
+	for i, k := range keySlice {
+		result[k] = append(result[k], valueSlice[i])
+	}
+	return result
 }
