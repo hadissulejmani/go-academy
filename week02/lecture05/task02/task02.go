80df5ba5c5cf9dccc9da5ea019590db69e8bfb14
Hadis Sulejmani <hadissulejmani@outlook.com>
2022-03-05 16:19:59 +0100

    Changes to lecture05/task02 after review

--- week02/lecture05/task02/task02.go
+++ week02/lecture05/task02/task02.go
@@ -10,23 +10,25 @@ type Card struct {
 func main() {
 	cardOne := Card{cardSuit: 3, cardValue: 3}
 	cardTwo := Card{cardSuit: 4, cardValue: 3}
+	cardThree := Card{cardSuit: 5, cardValue: 2}
 
 	cards := make([]Card, 5)
 	cards = append(cards, cardOne)
 	cards = append(cards, cardTwo)
+	cards = append(cards, cardThree)
 
 	resultFromFunction := maxCard(cards)
 	fmt.Println(resultFromFunction)
 }
 
 func maxCard(cards []Card) Card {
-	maxValue := 0
-	maxSuit := 0
+	maxCard := Card{cardSuit: 0, cardValue: 0}
 
-	for i, card := range cards {
-		if card.cardSuit >= maxSuit && card.cardValue > maxValue {
-			maxValue = i
+	for _, card := range cards {
+		if card.cardSuit >= maxCard.cardSuit && card.cardValue >= maxCard.cardValue {
+			maxCard.cardSuit = card.cardSuit
+			maxCard.cardValue = card.cardValue
 		}
 	}
-	return cards[maxValue]
+	return maxCard
 }
